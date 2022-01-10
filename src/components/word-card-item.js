911134c0f6d2640f575219f547ff1286a290c61d
caowei <caowei>
2022-01-10 10:28:57 +0800

    feat: master - add item

---
+++ src/components/word-card-item.js
@@ -0,0 +1,22 @@
+import moment from "moment";
+import {updateDescription} from "../service/datasource";
+import {Card, message, Typography} from "antd";
+import React from "react";
+
+const { Paragraph, Text } = Typography;
+
+export const WordCardItem = ({item})=>{
+
+  const [description, setDescription] = React.useState(item.description);
+
+  return <Card title={item.word}>
+    <Text type="secondary">{moment(item.createTime).format('YYYY-MM-DD HH:mm:ss')}</Text>
+    <Paragraph editable={{
+      onChange: async (value) => {
+        updateDescription(item.uid, value);
+        setDescription(value);
+        await message.success('修改成功');
+      }
+    }}>{description}</Paragraph>
+  </Card>
+}
--- src/components/word-card-list.js
+++ src/components/word-card-list.js
@@ -1,9 +1,7 @@
-import {Card, List, Typography, message} from "antd";
-import moment from "moment";
+import {List} from "antd";
 import React from "react";
-import {updateDescription} from "../service/datasource";
+import {WordCardItem} from "./word-card-item";
 
-const { Paragraph, Text } = Typography;
 
 
 export const WordCardList = (props)=>{
@@ -13,15 +11,7 @@ export const WordCardList = (props)=>{
     dataSource={dataSource}
     renderItem={item => (
       <List.Item>
-        <Card title={item.word}>
-          <Text type="secondary">{moment(item.createTime).format('YYYY-MM-DD HH:mm:ss')}</Text>
-          <Paragraph editable={{
-            onChange: async (value) => {
-              updateDescription(item.uid, value);
-              await message.success('修改成功');
-            }
-          }}>{item.description}</Paragraph>
-        </Card>
+        <WordCardItem item={item}/>
       </List.Item>
     )}
   />
