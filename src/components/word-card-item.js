74b31ccf4d628dc556ed24eb0f495dbacd4ca59a
caowei <caowei>
2022-03-01 09:16:40 +0800

    feat: feat/cw - update

--- src/components/word-card-item.js
+++ src/components/word-card-item.js
@@ -96,7 +96,7 @@ export const WordCardItem = ({ item: outerItem }) => {
               <Tag
                 key={3}
                 onClick={() => {
-                  fetch(`https://api.day.app/fBxLdXi5AcZV7QYbTb2vj6/${item.word}/${item.description}?sound=paymentsuccess&icon=https://avatars.githubusercontent.com/u/1531330?s=96&v=4`).then(()=>{
+                  fetch(`https://api.day.app/fBxLdXi5AcZV7QYbTb2vj6/${item.word}/${description}?sound=paymentsuccess&icon=https://avatars.githubusercontent.com/u/1531330?s=96&v=4`).then(()=>{
                     message.success(`"${item.word}" has been sent to your phone!`).then();
                   });
                 }}
